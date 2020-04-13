212aec2940556eee917607c5503a1fa284b1f420
nhung2409 <63304453+nhung2409@users.noreply.github.com>
2020-04-13 15:21:21 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Quanlythusech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMaTG = new System.Windows.Forms.TextBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.txtGioiTinh = new System.Windows.Forms.TextBox();
+            this.txtNgaySinh = new System.Windows.Forms.TextBox();
+            this.txtTenTG = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MaTG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenTG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NgaySinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã TG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Địa Chỉ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(494, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Giới Tính";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ngày Sinh";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(494, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Tên TG";
+            // 
+            // txtMaTG
+            // 
+            this.txtMaTG.Location = new System.Drawing.Point(99, 7);
+            this.txtMaTG.Name = "txtMaTG";
+            this.txtMaTG.Size = new System.Drawing.Size(100, 26);
+            this.txtMaTG.TabIndex = 6;
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(99, 114);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(100, 26);
+            this.txtDiaChi.TabIndex = 7;
+            // 
+            // txtGioiTinh
+            // 
+            this.txtGioiTinh.Location = new System.Drawing.Point(562, 57);
+            this.txtGioiTinh.Name = "txtGioiTinh";
+            this.txtGioiTinh.Size = new System.Drawing.Size(100, 26);
+            this.txtGioiTinh.TabIndex = 8;
+            // 
+            // txtNgaySinh
+            // 
+            this.txtNgaySinh.Location = new System.Drawing.Point(99, 57);
+            this.txtNgaySinh.Name = "txtNgaySinh";
+            this.txtNgaySinh.Size = new System.Drawing.Size(100, 26);
+            this.txtNgaySinh.TabIndex = 9;
+            // 
+            // txtTenTG
+            // 
+            this.txtTenTG.Location = new System.Drawing.Point(562, 6);
+            this.txtTenTG.Name = "txtTenTG";
+            this.txtTenTG.Size = new System.Drawing.Size(100, 26);
+            this.txtTenTG.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaTG,
+            this.TenTG,
+            this.NgaySinh,
+            this.DiaChi,
+            this.GioiTinh});
+            this.dataGridView1.Location = new System.Drawing.Point(2, 182);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(805, 150);
+            this.dataGridView1.TabIndex = 11;
+            
+            // 
+            // MaTG
+            // 
+            this.MaTG.DataPropertyName = "txtMaTG";
+            this.MaTG.HeaderText = "Mã TG";
+            this.MaTG.MinimumWidth = 8;
+            this.MaTG.Name = "MaTG";
+            this.MaTG.Width = 150;
+            // 
+            // TenTG
+            // 
+            this.TenTG.DataPropertyName = "txtTenTG";
+            this.TenTG.HeaderText = "Tên TG";
+            this.TenTG.MinimumWidth = 8;
+            this.TenTG.Name = "TenTG";
+            this.TenTG.Width = 150;
+            // 
+            // NgaySinh
+            // 
+            this.NgaySinh.DataPropertyName = "txtNgaySinh";
+            this.NgaySinh.HeaderText = "Ngày Sinh";
+            this.NgaySinh.MinimumWidth = 8;
+            this.NgaySinh.Name = "NgaySinh";
+            this.NgaySinh.Width = 150;
+            // 
+            // DiaChi
+            // 
+            this.DiaChi.DataPropertyName = "txtDiaChi";
+            this.DiaChi.HeaderText = "Địa Chỉ";
+            this.DiaChi.MinimumWidth = 8;
+            this.DiaChi.Name = "DiaChi";
+            this.DiaChi.Width = 150;
+            // 
+            // GioiTinh
+            // 
+            this.GioiTinh.DataPropertyName = "txtGioiTinh";
+            this.GioiTinh.HeaderText = "Giới Tính";
+            this.GioiTinh.MinimumWidth = 8;
+            this.GioiTinh.Name = "GioiTinh";
+            this.GioiTinh.Width = 150;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(49, 368);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 12;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(623, 368);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.TabIndex = 14;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(412, 368);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 15;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(222, 368);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 16;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtTenTG);
+            this.Controls.Add(this.txtNgaySinh);
+            this.Controls.Add(this.txtGioiTinh);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.txtMaTG);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMaTG;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.TextBox txtGioiTinh;
+        private System.Windows.Forms.TextBox txtNgaySinh;
+        private System.Windows.Forms.TextBox txtTenTG;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaTG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenTG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NgaySinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DiaChi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GioiTinh;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Quanlythusech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static SqlConnection conn = new SqlConnection();
+        public static string connectstring = "Data Source=LAPTOP-6SL7VIMI\\SQLEXPRESS;Initial Catalog=cuahangchothuesach;Integrated Security=True";
+        public static void ketnoi()
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = connectstring;
+            if (conn.State != ConnectionState.Open)
+            {
+                conn.Open();
+            }
+        }
+        public static void Dongketnoi()
+        {
+
+            if (conn.State != ConnectionState.Closed)
+            {
+                conn.Close();
+                conn.Dispose();
+                conn = null;
+            }
+        }
+        public static DataTable Docbang(string sql)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter mydata = new SqlDataAdapter();
+            mydata.SelectCommand = new SqlCommand();
+            ketnoi();
+            mydata.SelectCommand.Connection = conn;
+            mydata.SelectCommand.CommandText = sql;
+            mydata.Fill(dt);
+            Dongketnoi();
+            return dt;
+        }
+        public static void capnhat(string sql)
+        {
+            ketnoi();
+            SqlCommand sqlcomand = new SqlCommand();
+            sqlcomand.Connection = conn;
+            sqlcomand.CommandText = sql;
+            sqlcomand.ExecuteNonQuery();
+            Dongketnoi();
+        }
+        private void hienthi()
+        {
+            string sql;
+            DataTable tabletacgia;
+            sql = "select * from tacgia";
+            // dataGridView_Form1.DataSource = tabletacgia;
+            dataGridView1.Columns[0].HeaderText = " Mã tác giả";
+            dataGridView1.Columns[1].HeaderText = "Tên tác giả";
+            dataGridView1.Columns[2].HeaderText = "Ngày sinh";
+            dataGridView1.Columns[3].HeaderText = "Địa chỉ";
+           
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.EditMode = DataGridViewEditMode.EditProgrammatically;
+            // tabletacgia.Dispose();
+        }
+       
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            txtMaTG.Enabled = false;
+            txtTenTG.Enabled = false;
+            txtNgaySinh.Enabled = false;
+            txtDiaChi.Enabled = false;
+            txtGioiTinh.Enabled = false;
+            btnLuu.Enabled = false;
+            btnSua.Enabled = true;
+            btnThem.Enabled = true;
+            btnXoa.Enabled = true;
+
+        }
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            hienthi();
+        }
+
+        private void DataGridView_Form1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            //hienthi();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string sql;
+            //Form f = new Form();
+            //f.StartPosition = FormStartPosition.CenterScreen;
+            txtMaTG.Enabled = false;
+            txtTenTG.Enabled = false;
+            txtNgaySinh.Enabled = false;
+            txtDiaChi.Enabled = false;
+            txtGioiTinh.Enabled = false;
+            txtMaTG.Text = dataGridView1.CurrentRow.Cells["matacgia"].Value.ToString();
+            txtTenTG.Text = dataGridView1.CurrentRow.Cells["tentacgia"].Value.ToString();
+            txtNgaySinh.Text = dataGridView1.CurrentRow.Cells["ngaysinh"].Value.ToString();
+            txtDiaChi.Text = dataGridView1.CurrentRow.Cells["điachi"].Value.ToString();
+            txtGioiTinh.Text = dataGridView1.CurrentRow.Cells["gioitinh"].Value.ToString();
+
+            if (txtTenTG.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập tên tác giả!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtTenTG.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("bạn phải nhập tên tác giả!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtTenTG.Focus();
+                return;
+            }
+
+            // hienthi();
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (txtTenTG.Text == "" && txtTenTG.Text == "")
+            {
+                MessageBox.Show("Bạn không được để trống ", "Thông báo");
+                txtTenTG.Focus();
+            }
+            else
+            {
+                sql = "select * from tentacgia where Form1=N'" + txtTenTG.Text.Trim() + "'";
+                sql = "update tacgia set tentacgia=N'" + txtTenTG.Text.Trim() + "'where matacgia=N'" + txtMaTG.Text.Trim() + "'where ngaysinh=N" + txtNgaySinh.Text.Trim() + "whwwhere diachi=N'" + txtDiaChi.Text.Trim() + "'where gioitinh=N" + txtGioiTinh.Text.Trim();
+                capnhat(sql);
+                txtMaTG.Enabled = false;
+                txtTenTG.Enabled = false;
+                txtDiaChi.Enabled = false;
+                txtNgaySinh.Enabled = false;
+                txtGioiTinh.Enabled = false;
+                txtMaTG.Text = "";
+                txtTenTG.Text = "";
+                txtDiaChi.Text = "";
+                txtNgaySinh.Text = "";
+                txtGioiTinh.Text = "";
+            }
+        }
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (MessageBox.Show("Bạn có muốn xóa không?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+
+                sql = " delete tacgia where matacgia=N'" + txtMaTG.Text + "'";
+                capnhat(sql);
+                hienthi();
+            }
+        }
+    }
+}
+
